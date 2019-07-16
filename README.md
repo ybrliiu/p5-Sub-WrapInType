244f96c9ade796ffc5d59e0e08160aaa2c0e39a5
mp0liiu <raian@reeshome.org>
2019-07-16 23:09:41 +0900

    inittial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Sub::TypedAnon - It's new $module
+
+# SYNOPSIS
+
+    use Sub::TypedAnon;
+
+# DESCRIPTION
+
+Sub::TypedAnon is ...
+
+# LICENSE
+
+Copyright (C) mp0liiu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mp0liiu <raian@reeshome.org>
---
+++ lib/Sub/TypedAnon.pm
@@ -0,0 +1,39 @@
+package Sub::TypedAnon;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Sub::TypedAnon - It's new $module
+
+=head1 SYNOPSIS
+
+    use Sub::TypedAnon;
+
+=head1 DESCRIPTION
+
+Sub::TypedAnon is ...
+
+=head1 LICENSE
+
+Copyright (C) mp0liiu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mp0liiu E<lt>raian@reeshome.orgE<gt>
+
+=cut
+
